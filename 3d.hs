529ea2ee4323aa684ed2e49f151952a022e013e8
nitros12 <bsimms.simms@gmail.com>
2017-02-28 20:08:44 +0000

    stuffs

--- 3d.hs
+++ 3d.hs
@@ -1,18 +1,21 @@
-import          System.Console.Terminal.Size
+import           Data.Maybe                   (catMaybes, listToMaybe)
+import           System.Console.Terminal.Size
 
-data Coord = Coord Int Int deriving (Eq, Show) 
+data Coord = Coord Float Float deriving (Eq, Show)
 data Vec3d = Vec3d Float Float Float deriving (Eq, Show)
+data Tri2d = Tri2d Coord Coord Coord
 data Tri = Tri Vec3d Vec3d Vec3d
 data Transform = Transform Vec3d Vec3d -- translation, camera rotation
 
 type Mesh = [Tri]
+type FlatMesh = [Tri2d]
 type Zbuffer = [[(Char, Integer)]]
 
 add :: Vec3d -> Vec3d -> Vec3d
 (Vec3d a b c) `add` (Vec3d d e f) = Vec3d (a+d) (b+e) (c+f)
 
 rotate :: Vec3d -> Vec3d -> Vec3d
-rotate vec@(Vec3d x y z) (Vec3d rx ry rz) = 
+rotate vec@(Vec3d x y z) (Vec3d rx ry rz) =
     rotz (roty (rotx vec rx) ry) rz
 
 rotx :: Vec3d -> Float -> Vec3d
@@ -37,39 +40,68 @@ translateTri (Tri a b c) vec = Tri (a `add` vec) (b `add` vec) (c `add` vec)
 rotateTri :: Tri -> Vec3d -> Tri
 rotateTri (Tri a b c) vec = Tri (rotate a vec) (rotate b vec) (rotate c vec)
 
-translateMesh :: Mesh -> Vec3d -> Mesh
-translateMesh mesh vec = map (flip translateTri vec) mesh
+translateMesh :: Vec3d -> Mesh -> Mesh
+translateMesh vec = map (`translateTri` vec)
 
-rotateMesh :: Mesh -> Vec3d -> Mesh
-rotateMesh mesh vec = map (flip rotateTri vec) mesh
+rotateMesh :: Vec3d -> Mesh -> Mesh
+rotateMesh vec = map (`rotateTri` vec)
 
-transformMesh :: Mesh -> Vec3d -> Vec3d -> Mesh
-transformMesh mesh tran rot = (rotateMesh rot) . (translateMesh tran) mesh
+transformMesh :: Vec3d -> Vec3d -> Mesh -> Mesh
+transformMesh tran rot = rotateMesh rot . translateMesh tran
 
+project :: Vec3d -> Coord
+project (Vec3d x y z) = Coord (x/z) (y/z)
+
+projectTri :: Tri -> Tri2d
+projectTri (Tri a b c) = Tri2d (project a) (project b) (project c)
 
 depth :: Vec3d -> Float
 depth (Vec3d _ _ z) = z
 
-clamp :: (Num a) => a -> a -> a -> a
+depthTri :: Tri -> Coord -> Float
+depthTri (Tri
+  (Vec3d ax ay az)
+  (Vec3d bx by bz)
+  (Vec3d cx cy cz)) (Coord x y) =
+    (na / nb) * nc - (nd / ne) * nf
+      where na = (bx - ax)*(cz - az) - (cx - ax)*(bz - az)
+            nb = (bx - ax)*(cy - ay) - (cx - ax)*(by - ay)
+            nc = y - ay
+            nd = (by - ay)*(cz - az) - (cy - ay)*(bz - az)
+            ne = (bx - ax)*(cy - ay) - (cx - ax)*(by - ay)
+            nf = x - ax
+
+triSign :: Coord -> Coord -> Coord -> Float -- determine if a point is inside projected triangle
+triSign (Coord a b) (Coord c d) (Coord e f) = (a - e) * (d - f) - (c - e) * (b - f)
+
+triSignBool :: Tri2d -> Bool
+triSignBool (Tri2d a b c) = triSign a b c < 0.0
+
+coordInTri :: Tri2d -> Coord -> Bool
+coordInTri (Tri2d a b c) pt = (b1 == b2) && (b2 == b3)
+  where b1 = triSign pt a b
+        b2 = triSign pt b c
+        b3 = triSign pt c a
+
+coordIn3dTri :: Tri -> Coord -> Bool
+coordIn3dTri tri = coordInTri (projectTri tri)
+
+clamp :: (Ord a, Num a) => a -> a -> a -> a
 clamp i l m
     | i < l = l
     | i > m = m
     | otherwise = i
 
 
-zToChar :: Int -> Char
-zToChar z = "0XZVMYOAT8. "!!(clamp z 0 11)
+zToChar :: Float -> Char
+zToChar z = "0XZVMYOAT8. "!!clamp (round z) 0 11
 
---rasterize :: Mesh -> Coord -> Zbuffer
+flattenMesh :: Mesh -> FlatMesh
+flattenMesh = map projectTri
 
+getTriDept :: Tri -> Coord -> Coord -> [[Float]]
+getTriDept tri (Coord l b) (Coord r t) = [[depthTri tri (Coord x y) | x <- [l..r]] | y <- [b..t]]
 
-{-
-renderText :: [String] -> IO ()
-renderText s = do
-    clearScreen
-    map putStrLn s
-
-transformMesh :: Mesh -> Transform -> Mesh
-transformMesh shape (Transform tran rot) = 
--}
-
+rasterise :: [[Float]] -> [String]
+rasterise = map rasterline
+  where rasterline = map zToChar
